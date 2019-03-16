b10d759fef98e0fd12d87bf8134fbceb2cce2328
tom_gbl <goodMorning_glb@atguigu.com>
2019-03-16 18:07:41 +0800

    3-16

---
+++ src/index.js
@@ -0,0 +1,30 @@
+//console.log('home1111');
+//
+//class Log{
+//	constructor(){
+//		console.log('出错了');
+//	}
+//}
+//
+//let log=new Log();
+//import 'bootstrap';
+import './style';
+
+let url='';
+if(DEV==='dev'){
+	url="http://localhost:3000"
+}else{
+	url='http://www.zhufengpeixun.cn'
+}
+console.log(url);
+console.log(typeof FLAG);
+console.log(EXPORESSION);
+//let xhr=new XMLHttpRequest();
+//
+//xhr.open('GET','/user',true);
+//
+//xhr.onload=function(){
+//	console.log(xhr.response);
+//}
+//
+//xhr.send();
